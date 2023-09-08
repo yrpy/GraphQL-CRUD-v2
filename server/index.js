eb19e567d34dfab3471151c52dce63aa9d77a430
unknown <yravi.dt@gmail.com>
2023-09-08 19:12:41 +0530

    v2

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+require('dotenv').config()
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
+
+app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`))
